02ec6a3aae96247bbd4f9fd88eded259c321d70a
ploiu <ploiu415@gmail.com>
2022-08-30 19:54:02 -0400

    #4 - turned out this wasn't as big of a problem but now we're consistent

--- src/model/request/file_requests.rs
+++ src/model/request/file_requests.rs
@@ -6,7 +6,7 @@ pub struct CreateFileRequest<'a> {
     /// the file being uploaded
     pub file: TempFile<'a>,
     /// because I don't feel like mapping from content-type header
-    pub extension: &'a str,
+    pub extension: String,
     /// leave blank for top level folder
     pub folder_id: Option<u32>,
 }
