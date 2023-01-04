57fe93095845388f80dcfce90a29f8ddd167098a
ploiu <ploiu415@gmail.com>
2023-01-03 19:44:49 -0500

    update cert bs

--- gen_certs.sh
+++ gen_certs.sh
@@ -4,4 +4,4 @@ openssl genrsa -out private-key.pem 3072
 openssl rsa -in private-key.pem -pubout -out public-key.pem
 echo "Created Keys"
 echo "Generating a self-signed certificate. You may be prompted for input"
-openssl req -new -x509 -key private-key.pem -out cert.pem
+openssl req -new -x509 -key private-key.pem -out cert.pem -config cert_options.conf
