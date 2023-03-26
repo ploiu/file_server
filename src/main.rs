f4562f6d594de328ac62115b61e1b42f0861cccb
ploiu <ploiu415@gmail.com>
2023-03-25 21:54:36 -0400

    update api version for bug fix in tests merge

--- src/main.rs
+++ src/main.rs
@@ -58,7 +58,7 @@ mod api_tests {
         let client = Client::tracked(rocket()).expect("Valid Rocket Instance");
         let res = client.get(uri!("/api/version")).dispatch();
         assert_eq!(res.status(), Status::Ok);
-        assert_eq!(res.into_string().unwrap(), r#"{"version":"1.0.0"}"#);
+        assert_eq!(res.into_string().unwrap(), r#"{"version":"1.0.1"}"#);
     }
 
     #[test]
