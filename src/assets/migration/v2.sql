b527228b8ebe47c01b3d9db65913153d264f64fc
ploiu <ploiu415@gmail.com>
2023-10-24 17:48:46 -0400

    file_tags - v2 script for tags

---
+++ src/assets/migration/v2.sql
@@ -0,0 +1,33 @@
+begin;
+
+create table Tags
+(
+    id    integer primary key autoincrement,
+    title varchar not null unique
+);
+
+create table FileRecords_Tags
+(
+    fileRecordTagId integer primary key autoincrement,
+    fileRecordId    integer,
+    tagId           integer,
+    foreign key (fileRecordId) references FileRecords (id),
+    foreign key (tagId) references Tags (id),
+    unique (fileRecordId, tagId)
+);
+
+create table Folders_Tags
+(
+    foldersTagId integer primary key autoincrement,
+    folderId     integer,
+    tagId        integer,
+    foreign key (folderId) references Folders (id),
+    foreign key (tagId) references Tags (id),
+    unique (folderId, tagId)
+);
+
+update Metadata
+set value = '2'
+where name = 'version';
+
+commit;
