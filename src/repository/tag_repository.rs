d4ca9335b23485bd648b729cbd1e78343d08846d
ploiu <ploiu415@gmail.com>
2023-11-04 17:35:54 -0400

    file_tags - finish repository layer

--- src/repository/tag_repository.rs
+++ src/repository/tag_repository.rs
@@ -1,4 +1,3 @@
-use rocket::http::ext::IntoCollection;
 use rusqlite::Connection;
 
 use crate::model::repository;
@@ -6,9 +5,7 @@ use crate::model::repository;
 /// creates a new tag in the database. This does not check if the tag already exists,
 /// so the caller must check that themselves
 pub fn create_tag(title: String, con: &Connection) -> Result<repository::Tag, rusqlite::Error> {
-    let mut pst = con
-        .prepare(include_str!("../assets/queries/tags/create_tag.sql"))
-        .unwrap();
+    let mut pst = con.prepare(include_str!("../assets/queries/tags/create_tag.sql"))?;
     let id = pst.insert(rusqlite::params![title])? as u32;
     Ok(repository::Tag { id, title })
 }
@@ -20,9 +17,7 @@ pub fn get_tag_by_title(
     title: String,
     con: &Connection,
 ) -> Result<Option<repository::Tag>, rusqlite::Error> {
-    let mut pst = con
-        .prepare(include_str!("../assets/queries/tags/get_by_title.sql"))
-        .unwrap();
+    let mut pst = con.prepare(include_str!("../assets/queries/tags/get_by_title.sql"))?;
     return match pst.query_row(rusqlite::params![title], tag_mapper) {
         Ok(tag) => Ok(Some(tag)),
         Err(e) => {
@@ -38,43 +33,35 @@ pub fn get_tag_by_title(
 }
 
 pub fn get_tag(id: u32, con: &Connection) -> Result<repository::Tag, rusqlite::Error> {
-    let mut pst = con
-        .prepare(include_str!("../assets/queries/tags/get_by_id.sql"))
-        .unwrap();
+    let mut pst = con.prepare(include_str!("../assets/queries/tags/get_by_id.sql"))?;
     return Ok(pst.query_row(rusqlite::params![id], tag_mapper)?);
 }
 
 /// updates the past tag. Checking to make sure the tag exists needs to be done on the caller's end
 pub fn update_tag(tag: repository::Tag, con: &Connection) -> Result<(), rusqlite::Error> {
-    let mut pst = con
-        .prepare(include_str!("../assets/queries/tags/update_tag.sql"))
-        .unwrap();
+    let mut pst = con.prepare(include_str!("../assets/queries/tags/update_tag.sql"))?;
     pst.execute(rusqlite::params![tag.title, tag.id])?;
     Ok(())
 }
 
 pub fn delete_tag(id: u32, con: &Connection) -> Result<(), rusqlite::Error> {
-    let mut pst = con
-        .prepare(include_str!("../assets/queries/tags/delete_tag.sql"))
-        .unwrap();
+    let mut pst = con.prepare(include_str!("../assets/queries/tags/delete_tag.sql"))?;
     pst.execute(rusqlite::params![id])?;
     Ok(())
 }
 
-/// the caller of this function will need to make sure the tag already exists,
-/// but this function makes sure the tag isn't on the file already
+/// the caller of this function will need to make sure the tag already exists and isn't already on the file
 pub fn add_tag_to_file(file_id: u32, tag_id: u32, con: &Connection) -> Result<(), rusqlite::Error> {
-    panic!("unimplemented");
+    let mut pst = con.prepare(include_str!("../assets/queries/tags/add_tag_to_file.sql"))?;
+    pst.execute(rusqlite::params![file_id, tag_id])?;
+    Ok(())
 }
 
-// TODO tests when my brain isn't melting
 pub fn get_tags_on_file(
     file_id: u32,
     con: &Connection,
 ) -> Result<Vec<repository::Tag>, rusqlite::Error> {
-    let mut pst = con
-        .prepare(include_str!("../assets/queries/tags/get_tags_for_file.sql"))
-        .unwrap();
+    let mut pst = con.prepare(include_str!("../assets/queries/tags/get_tags_for_file.sql"))?;
     let rows = pst.query_map(rusqlite::params![file_id], |row| Ok(tag_mapper(&row)))?;
     let mut tags: Vec<repository::Tag> = Vec::new();
     for tag_res in rows {
@@ -83,6 +70,57 @@ pub fn get_tags_on_file(
         tags.push(tag_res??);
     }
     Ok(tags)
+}
+
+pub fn remove_tag_from_file(
+    file_id: u32,
+    tag_id: u32,
+    con: &Connection,
+) -> Result<(), rusqlite::Error> {
+    let mut pst = con.prepare(include_str!(
+        "../assets/queries/tags/remove_tag_from_file.sql"
+    ))?;
+    pst.execute(rusqlite::params![file_id, tag_id])?;
+    Ok(())
+}
+
+pub fn add_tag_to_folder(
+    folder_id: u32,
+    tag_id: u32,
+    con: &Connection,
+) -> Result<(), rusqlite::Error> {
+    let mut pst = con.prepare(include_str!("../assets/queries/tags/add_tag_to_folder.sql"))?;
+    pst.execute(rusqlite::params![folder_id, tag_id])?;
+    Ok(())
+}
+
+pub fn get_tags_on_folder(
+    folder_id: u32,
+    con: &Connection,
+) -> Result<Vec<repository::Tag>, rusqlite::Error> {
+    let mut pst = con.prepare(include_str!(
+        "../assets/queries/tags/get_tags_for_folder.sql"
+    ))?;
+    let rows = pst.query_map(rusqlite::params![folder_id], |row| Ok(tag_mapper(&row)))?;
+    let mut tags: Vec<repository::Tag> = Vec::new();
+    for tag_res in rows {
+        // I know it's probably bad style, but I'm laughing too hard at the double question mark.
+        // no I don't know what my code is doing and I'm glad my code reflects that
+        tags.push(tag_res??);
+    }
+    Ok(tags)
+}
+
+pub fn remove_tag_from_folder(
+    folder_id: u32,
+    tag_id: u32,
+    con: &Connection,
+) -> Result<(), rusqlite::Error> {
+    let mut pst = con.prepare(include_str!(
+        "../assets/queries/tags/remove_tag_from_folder.sql"
+    ))?;
+    pst.execute(rusqlite::params![folder_id, tag_id])?;
+    Ok(())
 }
 
 fn tag_mapper(row: &rusqlite::Row) -> Result<repository::Tag, rusqlite::Error> {
@@ -106,7 +144,7 @@ mod create_tag_tests {
         assert_eq!(
             Tag {
                 id: 1,
-                title: "test".to_string()
+                title: "test".to_string(),
             },
             tag
         );
@@ -130,7 +168,7 @@ mod get_tag_by_title_tests {
         assert_eq!(
             Some(Tag {
                 id: 1,
-                title: "test".to_string()
+                title: "test".to_string(),
             }),
             found
         );
@@ -163,7 +201,7 @@ mod get_tag_by_id_tests {
         assert_eq!(
             Tag {
                 id: 1,
-                title: "test".to_string()
+                title: "test".to_string(),
             },
             tag
         );
@@ -195,7 +233,7 @@ mod update_tag_tests {
         assert_eq!(
             Tag {
                 id: 1,
-                title: "test2".to_string()
+                title: "test2".to_string(),
             },
             res
         );
@@ -217,5 +255,184 @@ mod delete_tag_tests {
         let not_found = get_tag(1, &con);
         con.close().unwrap();
         assert_eq!(Err(rusqlite::Error::QueryReturnedNoRows), not_found);
+    }
+}
+
+#[cfg(test)]
+mod get_tag_on_file_tests {
+    use crate::model::repository::{FileRecord, Tag};
+    use crate::repository::file_repository::create_file;
+    use crate::repository::open_connection;
+    use crate::repository::tag_repository::{add_tag_to_file, create_tag, get_tags_on_file};
+    use crate::test::refresh_db;
+
+    #[test]
+    fn get_tags_on_file_returns_tags() {
+        refresh_db();
+        let con = open_connection();
+        create_tag("test".to_string(), &con).unwrap();
+        create_tag("test2".to_string(), &con).unwrap();
+        create_file(
+            &FileRecord {
+                id: None,
+                name: "test_file".to_string(),
+            },
+            &con,
+        )
+        .unwrap();
+        add_tag_to_file(1, 1, &con).unwrap();
+        add_tag_to_file(1, 2, &con).unwrap();
+        let res = get_tags_on_file(1, &con).unwrap();
+        con.close().unwrap();
+        assert_eq!(
+            vec![
+                Tag {
+                    id: 1,
+                    title: "test".to_string()
+                },
+                Tag {
+                    id: 2,
+                    title: "test2".to_string()
+                }
+            ],
+            res
+        );
+    }
+
+    #[test]
+    fn get_tags_on_file_returns_nothing_if_no_tags() {
+        refresh_db();
+        let con = open_connection();
+        create_file(
+            &FileRecord {
+                id: None,
+                name: "test_file".to_string(),
+            },
+            &con,
+        )
+        .unwrap();
+        let res = get_tags_on_file(1, &con).unwrap();
+        con.close().unwrap();
+        assert_eq!(Vec::<Tag>::new(), res);
+    }
+}
+
+#[cfg(test)]
+mod remove_tag_from_file_tests {
+    use crate::model::repository::{FileRecord, Tag};
+    use crate::repository::file_repository::create_file;
+    use crate::repository::open_connection;
+    use crate::repository::tag_repository::{create_tag, get_tags_on_file, remove_tag_from_file};
+    use crate::test::refresh_db;
+
+    #[test]
+    fn remove_tag_from_file_works() {
+        refresh_db();
+        let con = open_connection();
+        create_tag("test".to_string(), &con).unwrap();
+        create_file(
+            &FileRecord {
+                id: None,
+                name: "test_file".to_string(),
+            },
+            &con,
+        )
+        .unwrap();
+        remove_tag_from_file(1, 1, &con).unwrap();
+        let tags = get_tags_on_file(1, &con).unwrap();
+        con.close().unwrap();
+        assert_eq!(Vec::<Tag>::new(), tags);
+    }
+}
+
+#[cfg(test)]
+mod get_tag_on_folder_tests {
+    use crate::model::repository::{Folder, Tag};
+    use crate::repository::folder_repository::create_folder;
+    use crate::repository::open_connection;
+    use crate::repository::tag_repository::{add_tag_to_folder, create_tag, get_tags_on_folder};
+    use crate::test::refresh_db;
+
+    #[test]
+    fn get_tags_on_folder_returns_tags() {
+        refresh_db();
+        let con = open_connection();
+        create_tag("test".to_string(), &con).unwrap();
+        create_tag("test2".to_string(), &con).unwrap();
+        create_folder(
+            &Folder {
+                parent_id: None,
+                id: None,
+                name: "test_folder".to_string(),
+            },
+            &con,
+        )
+        .unwrap();
+        add_tag_to_folder(1, 1, &con).unwrap();
+        add_tag_to_folder(1, 2, &con).unwrap();
+        let res = get_tags_on_folder(1, &con).unwrap();
+        con.close().unwrap();
+        assert_eq!(
+            vec![
+                Tag {
+                    id: 1,
+                    title: "test".to_string()
+                },
+                Tag {
+                    id: 2,
+                    title: "test2".to_string()
+                }
+            ],
+            res
+        );
+    }
+
+    #[test]
+    fn get_tags_on_folder_returns_nothing_if_no_tags() {
+        refresh_db();
+        let con = open_connection();
+        create_folder(
+            &Folder {
+                parent_id: None,
+                id: None,
+                name: "test_folder".to_string(),
+            },
+            &con,
+        )
+        .unwrap();
+        let res = get_tags_on_folder(1, &con).unwrap();
+        con.close().unwrap();
+        assert_eq!(Vec::<Tag>::new(), res);
+    }
+}
+
+#[cfg(test)]
+mod remove_tag_from_folder_tests {
+    use crate::model::repository::{Folder, Tag};
+    use crate::repository::folder_repository::create_folder;
+    use crate::repository::open_connection;
+    use crate::repository::tag_repository::{
+        create_tag, get_tags_on_folder, remove_tag_from_folder,
+    };
+    use crate::test::refresh_db;
+
+    #[test]
+    fn remove_tag_from_folder_works() {
+        refresh_db();
+        let con = open_connection();
+        create_tag("test".to_string(), &con).unwrap();
+        create_folder(
+            &Folder {
+                parent_id: None,
+                id: None,
+                name: "test_folder".to_string(),
+            },
+            &con,
+        )
+        .unwrap();
+        remove_tag_from_folder(1, 1, &con).unwrap();
+        let tags = get_tags_on_folder(1, &con).unwrap();
+        con.close().unwrap();
+        assert_eq!(Vec::<Tag>::new(), tags);
     }
 }
