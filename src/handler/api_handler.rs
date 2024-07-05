9dadecb261cc704e54fe0b160518e81c710a06ff
ploiu <ploiu415@gmail.com>
2024-07-05 15:08:47 -0400

    version bump

--- src/handler/api_handler.rs
+++ src/handler/api_handler.rs
@@ -6,7 +6,7 @@ use crate::model::response::api_responses::{SetPassWordResponse, UpdatePasswordR
 use crate::model::response::BasicMessage;
 use crate::service::api_service;
 
-static API_VERSION_NUMBER: &str = "2.5.2";
+static API_VERSION_NUMBER: &str = "2.6.0";
 
 #[derive(Serialize)]
 #[serde(crate = "rocket::serde")]
