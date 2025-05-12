929bd15dc3dcec3190ea645235bb475962b4e0c6
ploiu <ploiu415@gmail.com>
2025-05-12 19:40:59 -0400

    Revert "some small updates"
    This reverts commit bc3db649f0f6e2e2e9c9e8f3b3c85ef1b85b9ec4.

--- gen_certs.sh
+++ gen_certs.sh
@@ -1,14 +1,6 @@
 # will generate certs for you and place them here so you don't have to do so manually
-echo "generating private root ca key"
-openssl genrsa -out ca.key 3072
-echo "generating root ca cert"
-openssl req -x509 -new -nodes -key ca.key -sha256 -days 30000 -out ca.crt -config ./cert_options.conf
-echo "Generating private key"
+echo "Generating public and private keys..."
 openssl genrsa -out private-key.pem 3072
-echo "generating server signing request"
-openssl req -new -key private-key.pem -out server.csr -config cert_options.conf
-echo "signing server csr with ca certificate"
-openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out cert.pem -days 30000 -sha256 -extfile cert_options.conf -extensions v3_req
 openssl rsa -in private-key.pem -pubout -out public-key.pem
-echo "combining server cert and key for mobile devices"
-openssl pkcs12 -export -out file_server.p12 -inkey private-key.pem -in cert.pem -certfile ca.crt
+echo "Created Keys"
+openssl req -new -x509 -key private-key.pem -out cert.pem -config cert_options.conf
